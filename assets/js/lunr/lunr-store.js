21762c356444360600bb6f22e1bf802be022681a
Samuel SAYAG <samuel.sayag@clicktale.com>
2021-01-28 08:56:54 +0200

    init

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Bash Notes - Part 1",
+        "excerpt":"Introduction Not so long ago, I decided I needed to learn Bash more seriously. I already coded a bit in Bash, but all my knowledge was made of meaning inferrence from code in various blogs and StackOverflow etc. While this is a perfect way to learn when you want practical...","categories": [],
+        "tags": [],
+        "url": "htps://samuelsayag.github.io/bash/notes/1",
+        "teaser": "htps://samuelsayag.github.io/assets/images/romanesco-5020778_640.jpg"
+      }]
