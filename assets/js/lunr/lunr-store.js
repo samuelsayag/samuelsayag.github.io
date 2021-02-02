815e55ef2ff02ca61dc90dec14e4a2290ca7501b
Samuel SAYAG <samuel.sayag@clicktale.com>
2021-02-02 12:10:05 +0200

    second post

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,7 +1,13 @@
 var store = [{
         "title": "Bash Notes - Part 1",
-        "excerpt":"Introduction Not so long ago, I decided I needed to learn Bash more seriously. I already coded a bit in Bash, but all my knowledge was made of meaning inferrence from code in various blogs and StackOverflow etc. While this is a perfect way to learn when you want practical...","categories": [],
-        "tags": [],
+        "excerpt":"Introduction Not so long ago, I decided I needed to learn Bash more seriously. I already coded a bit in Bash, but all my knowledge was made of examples inferrence from various code encountered in blogs and StackOverflow etc. Why learning bash (as a software developer) and not Python for...","categories": [],
+        "tags": ["script","bash","variable","execution","argument","substitution","here document","read","shift"],
         "url": "htps://samuelsayag.github.io/bash/notes/1",
+        "teaser": "htps://samuelsayag.github.io/assets/images/romanesco-5020778_640.jpg"
+      },{
+        "title": "Bash Notes - Part 2",
+        "excerpt":"Bash Notes (Part 2) - Transforming the input Introduction In the first part, I introduced the reader to the basic of writing scripts (shebang, comments, parameters, variable and quoting…). This second part focuses on processing input parameter or variables of a script: checking parameters, providing default, extracting part via pattern/regexp...","categories": [],
+        "tags": ["pattern","matching","pattern matching","regular","expression","regular expression","parameter expansion","variable expansion"],
+        "url": "htps://samuelsayag.github.io/bash/notes/2",
         "teaser": "htps://samuelsayag.github.io/assets/images/romanesco-5020778_640.jpg"
       }]
