fb99166be72d211c74a346b8eac9ea3c79af0c94
Samuel SAYAG <samuel.sayag@clicktale.com>
2021-07-23 09:07:42 +0300

    update + add variance distilled

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,12 +2,18 @@ var store = [{
         "title": "Bash Notes - Part 1",
         "excerpt":"Introduction Not so long ago, I decided I needed to learn Bash more seriously. I already coded a bit in Bash, but all my knowledge was made of examples inferrence from various code encountered in blogs and StackOverflow etc. Why learning bash (as a software developer) and not Python for...","categories": [],
         "tags": ["script","bash","variable","execution","argument","substitution","here document","read","shift"],
-        "url": "htps://samuelsayag.github.io/bash/notes/1",
-        "teaser": "htps://samuelsayag.github.io/assets/images/romanesco-5020778_640.jpg"
+        "url": "/bash/notes/1",
+        "teaser": "/assets/images/romanesco-5020778_640.jpg"
       },{
         "title": "Bash Notes - Part 2",
         "excerpt":"Bash Notes (Part 2) - Transforming the input Introduction In the first part, I introduced the reader to the basic of writing scripts (shebang, comments, parameters, variable and quoting…). This second part focuses on processing input parameter or variables of a script: checking parameters, providing default, extracting part via pattern/regexp...","categories": [],
         "tags": ["pattern","matching","pattern matching","regular","expression","regular expression","parameter expansion","variable expansion"],
-        "url": "htps://samuelsayag.github.io/bash/notes/2",
-        "teaser": "htps://samuelsayag.github.io/assets/images/romanesco-5020778_640.jpg"
+        "url": "/bash/notes/2",
+        "teaser": "/assets/images/romanesco-5020778_640.jpg"
+      },{
+        "title": "Variance Distilled",
+        "excerpt":"Scala Variance Distilled Introduction This article could have been called YAASV (“Yet Another Article on Scala Variance”). There is already a lot of (good!) resources about variance out there (more about that in the next section). So why making another article about it? Well, because if reading these resources gives...","categories": [],
+        "tags": ["scala","variance","subtyping","type inference","type","inference","ZIO"],
+        "url": "/scala/variance/variance-distilled",
+        "teaser": "/assets/images/romanesco-5020778_640.jpg"
       }]
